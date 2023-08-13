7a341c902d39c666549dfbf470eb8be9157c739f
Alexander Demin <ademin@gmail.com>
2023-08-13 19:43:50 +0100

    Create types.ts

---
+++ mamabot/types.ts
@@ -0,0 +1,5 @@
+import type { ParseMode } from "npm:@types/node-telegram-bot-api";
+import { Audios } from "./audios.ts";
+import { Buttons } from "./buttons.ts";
+import { Images } from "./images.ts";
+import { Messages } from "./messages.ts";
