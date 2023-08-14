d92665fe041590c8b3cbb93d59d6a06d1d5f8fab
Alexander Demin <ademin@gmail.com>
2023-08-14 01:54:12 +0100

    Update 2023-08-14T00:54:12.248Z

--- mamabot/types.ts
+++ mamabot/types.ts
@@ -3,3 +3,5 @@ import { Audios } from "./audios.ts";
 import { Buttons } from "./buttons.ts";
 import { Images } from "./images.ts";
 import { Messages } from "./messages.ts";
+
+// 2023-08-14T00:54:12.065Z
