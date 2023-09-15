9ece950fe5da980b17632b2efc5918600fb9a3fe
Alexander Demin <ademin@gmail.com>
2023-09-15 06:44:07 +0100

    Fix advices

---
+++ fix-check.js
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+const ru = JSON.parse(fs.readFileSync("dashok/ru/database.json").toString());
+const uk = JSON.parse(fs.readFileSync("dashok/uk/database.json").toString());
+
+if (uk.advices.length !== ru.advices.length)
+    throw new Error("uk.advices.length !== ru.advices.length");
